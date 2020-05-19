f04f3f2e1e1820b761be656fe368fa6b084d9d61
Bailey <baileylritchie@gmail.com>
2020-05-19 12:40:27 -0400

    deployment 1

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,11 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Hi, I'm Bailey! 👋</p>
+        <p>I'm a full stack software developer passionate about all things web. </p>
         <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          This is a sample website - built from a{' '}
+          <a href="https://nextjs.org/learn">Next.js tutorial</a>.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
